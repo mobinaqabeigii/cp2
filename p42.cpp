0583b4e45fcb4dfc99607f3886da3bddcfb1e46d
Mbina <117299384+mobinaqabeigii@users.noreply.github.com>
2022-11-07 03:37:58 +0330

    p42

---
+++ p42.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+   main() {
+   int x,y,z,g,w,r;
+ 	cin>>x>>y>>z;
+ 	r=(x-y)*(x+y);
+ 	cout<<r;
+ 	w=((x+y+z)*(x+y+z))+1;
+ 	cout<<w;
+ 	g=r/g;
+ 	cout<<"finally"<<g;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+}
